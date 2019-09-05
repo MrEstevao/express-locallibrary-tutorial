7d217efef99d5db6103596206411454d3c0d5dc3
Estevão <estevao.lima@live.com>
2019-09-05 14:57:45 -0400

    problem with connection

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var app = express();
 app.use(helmet());
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://mongoAdmin:mozillaCourse@cluster0-ntyds.mongodb.net/local_library?retryWrites=true';
-//var dev_db_url = 'mongodb+srv://mongoAdmin:mozillaCourse@cluster0-ntyds.mongodb.net/local_library?retryWrites=true&w=majority';
-//var mongoDB = process.env.MONGODB_URI || dev_db_url;
+//var mongoDB = 'mongodb+srv://mongoAdmin:mozillaCourse@cluster0-ntyds.mongodb.net/local_library?retryWrites=true';
+var dev_db_url = 'mongodb+srv://mongoAdmin:mozillaCourse@cluster0-ntyds.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser:true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
